fee1a08d8b0cb561124e0516ef685f626cd9a007
Jonathan Kilzi <jonathan.kilzi@888holdings.com>
2020-08-02 18:58:18 +0300

    chore: updates issue in install-deps script

--- ci/scripts/install-dependencies.sh
+++ ci/scripts/install-dependencies.sh
@@ -8,18 +8,18 @@ KUBECTL_VERSION="v1.16.6-beta.0"
 DOCKER_COMPOSE_VERSION_REGEX=$(echo $DOCKER_COMPOSE_VERSION | sed -E 's/\./\\\./g')
 if [[ $1 = "ci" ]] && [[ ! $(docker-compose --version) =~ $DOCKER_COMPOSE_VERSION_REGEX ]]
 then
-    sudo rm /usr/local/bin/docker-compose
-    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
-    chmod +x docker-compose
+    sudo rm /usr/local/bin/docker-compose && \
+    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose && \
+    chmod +x docker-compose && \
     sudo mv docker-compose /usr/local/bin
 fi
-docker-compose --version
+# docker-compose --version
 
 KUBECTL_VERSION_REGEX=$(echo $KUBECTL_VERSION | sed -E 's/\./\\\./g')
 if [[ $1 = "ci" ]] && [[ ! $(kubectl version --client --short) =~ $KUBECTL_VERSION_REGEX ]]
 then
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
-    chmod +x ./kubectl
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl && \
+    chmod +x ./kubectl && \
     sudo mv ./kubectl /usr/local/bin/kubectl
 fi
-kubectl version --client --short
+# kubectl version --client --short
