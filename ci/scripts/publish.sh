2ecef14da66ea4020bb9e2521e24cd13747e12f0
Jonathan Kilzi <jonathan.kilzi@888holdings.com>
2020-08-02 18:41:49 +0300

    chore: Updates the scripts

--- ci/scripts/publish.sh
+++ ci/scripts/publish.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-docker push y0n1/udagram-feed-api && \
-docker push y0n1/udagram-user-api && \
-docker push y0n1/udagram-reverse-proxy && \
+docker push y0n1/udagram-feed-api &
+docker push y0n1/udagram-user-api &
+docker push y0n1/udagram-reverse-proxy &
 docker push y0n1/udagram-web-client
